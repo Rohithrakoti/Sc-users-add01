5cfc8b877be7debe821ea0fbaf29ad1d9782849c
Rohith <118875691+Rohithrakoti@users.noreply.github.com>
2026-07-22 14:38:06 +0530

    Add payload dictionary for query parameters

---
+++ Pylod.py
@@ -0,0 +1,16 @@
+payload = {
+    "query": group_name,
+    "identityTypes": [
+        "group",
+        "team"
+    ],
+    "operationScopes": [
+        "ims",
+        "ad",
+        "vmd"
+    ],
+    "options": {
+        "MinResults": 10,
+        "MaxResults": 10
+    }
+}
